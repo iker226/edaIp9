5e1287c3eed5e3836046c2361227e166a40ef3fc
Esquivel Cortés Iker Alonso <ikering226@gmail.com>
2026-05-08 16:23:50 -0600

    Update programa5.py

---
+++ programa5.py
@@ -0,0 +1,38 @@
+#SUMA
+print ("\n SUMA")
+x = int(input("Ingresa el valor de x: "))
+y = int(input("Ingresa el valor de y: "))
+print ("La suma de los numeros es: {}".format(x + y))
+
+#RESTA
+print ("\n RESTA")
+x = int(input("Ingresa el valor de x: "))
+y = int(input("Ingresa el valor de y: "))
+print ("La resta de los numeros es: {}".format(x - y))
+
+#MULTIPLICACION
+print ("\n MULTIPLICACION")
+x = int(input("Ingresa el valor de x: "))
+y = int(input("Ingresa el valor de y: "))
+print ("La multiplicacion de los numeros es: {}".format(x * y))
+
+#DIVISION
+print ("\n DIVISION")
+x = float(input("Ingresa el valor de x: "))
+y = float(input("Ingresa el valor de y: "))
+print ("La division de los numeros es: {}".format(x / y))
+
+#POTENCIA, DIVISION ENTRE ENTEROS, Y MODULO
+n1 = int(input("Ingresa el primer valor: "))
+n2 = int(input("Ingresa el segundo valor: "))
+print("{}^{} = {}".format(n1, n2, n1**n2))
+print("{}/{} = {}".format(n1, n2, n1//n2))  #// Es division entera
+print("{} mod {} = {}".format(n1, n2, n1%n2))
+
+#COMPARACI0N
+print("{} < {} = {}".format(n1, n2, n1<n2))
+print("{} > {} = {}".format(n1, n2, n1>n2))
+print("{} >= {} = {}".format(n1, n2, n1>=n2))
+print("{} <= {} = {}".format(n1, n2, n1<=n2))
+print("{} == {} = {}".format(n1, n2, n1==n2))
+print("{} != {} = {}".format(n1, n2, n1!=n2))
