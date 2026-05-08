574ee63c18a85861ce08e0c629decf2556008c6e
Esquivel Cortés Iker Alonso <ikering226@gmail.com>
2026-05-08 16:29:25 -0600

    Update tupla.py

---
+++ tupla.py
@@ -0,0 +1,16 @@
+diasdeMes= (31,28,31,30,31,30,31,31,30,31,30,31)
+
+print(diasdeMes)
+print("Enero tiene {} días".format(diasdeMes[0]))
+print("Julio tiene {} días".format(diasdeMes[6]))
+print("Agosto tiene {} días".format(diasdeMes[7]))
+print("Diciembre tiene {} días".format(diasdeMes[11]))
+
+#diasdeMes[1] = 29
+#print("Febrero tiene {} días".format(diasdeMes[1]))
+
+#diasdeMes.insert(1,2) #diasdeMes[1] = 2 (Es equivalente)
+#print(diasdeMes)
+
+#diasdeMes.extend((2,3,400))
+#print(diasdeMes)
