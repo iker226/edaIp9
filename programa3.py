3ab809829010f50eba381780059062a747038541
Esquivel Cortés Iker Alonso <ikering226@gmail.com>
2026-05-08 16:22:14 -0600

    Update programa3.py

---
+++ programa3.py
@@ -0,0 +1,9 @@
+str1 = "hola"
+str2 = "mundo"
+str3 = "en"
+str4 = "python"
+
+str5 = str1 + " " + str2 + " " + str3 + ' ' + str4
+
+print(str5)
+print("{} {} {} {}". format(str1, str2, str3, str4))
