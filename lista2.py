b55ff4894bdff822a08df59c16677520d14ec318
Esquivel Cortés Iker Alonso <ikering226@gmail.com>
2026-05-08 16:28:44 -0600

    Update lista2.py

---
+++ lista2.py
@@ -0,0 +1,9 @@
+diasdeMes= [["Enero",31],["Febrero",28],["Marzo",31],["Abril",30],["Mayo",31],
+["Junio",30],["Julio",31],["Agosto",31],["Septiembre",30],["Octubre",31],
+["Noviembre",30],["Diciembre",31]]
+
+print(diasdeMes)
+print("{} tiene {} días".format(diasdeMes[0][0], diasdeMes[0][1]))
+print("{} tiene {} días".format(diasdeMes[6][0], diasdeMes[6][1]))
+print("{} tiene {} días".format(diasdeMes[7][0], diasdeMes[7][1]))
+print("{} tiene {} días".format(diasdeMes[11][0], diasdeMes[11][1]))
